f73b395697fffdff415d65acfe5de905d78a9c88
Josepe Gallicchio <157397890+Josepega@users.noreply.github.com>
2024-03-18 11:10:22 +0100

    Add files via upload

---
+++ FRONTEND/js/menu_izq.js
@@ -0,0 +1,7 @@
+const botonToggle = document.querySelector('.toggle_boton');
+
+botonToggle.addEventListener('click', () => {
+    document.getElementById('izq').classList.toggle('active');
+
+    
+})
