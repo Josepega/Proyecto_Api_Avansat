3ce70c9dbd632e01704615947b1e2a601b8aa990
Josepega <josepegalli@gmail.com>
2024-03-23 22:06:02 +0100

    alta clientes ok

--- BACKEND/database/Avansat_DB.sql
+++ BACKEND/database/Avansat_DB.sql
@@ -22,24 +22,22 @@ USE `avansat_db` ;
 -- Table `avansat_db`.`clientes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `avansat_db`.`clientes` (
-  `id_cliente` INT NOT NULL AUTO_INCREMENT,
+  `Id_cliente` INT auto_increment ,
   `Nombre` VARCHAR(255) NULL DEFAULT NULL,
   `Apellidos` VARCHAR(255) NULL DEFAULT NULL,
-  `Nif` VARCHAR(9) NULL DEFAULT NULL,
-  `Cif` VARCHAR(9) NULL DEFAULT NULL,
+  `Id_fiscal` VARCHAR(9) NULL DEFAULT NULL,
   `Direccion` VARCHAR(255) NULL DEFAULT NULL,
+  `C.P` INT(5) NULL,
+  `Localidad` VARCHAR(45) NULL,
+  `Pais` VARCHAR(45) NULL,
   `Telefono` VARCHAR(15) NULL DEFAULT NULL,
   `Movil` VARCHAR(15) NULL DEFAULT NULL,
   `Email` VARCHAR(45) NULL DEFAULT NULL,
-  PRIMARY KEY (`id_cliente`))
+  PRIMARY KEY (`Id_cliente`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-
+SELECT * FROM clientes;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-
-
-
--- BACKEND/routes/app.routes.js
+++ BACKEND/routes/app.routes.js
@@ -25,17 +25,19 @@ router.get('/Saludo', async (req, res) => {
 router.post('/alta_cliente', (req, res) => {
 try {
     const id_clientes = req.body.id_clientes;
-    const nombre = req.body.Nombre;
-    const apellidos = req.body.Apellidos;
-    const nif = req.body.Nif;
-    const cif = req.body.Cif;
-    const direccion = req.body.Direccion;
-    const telefono = req.body.Telefono;
-    const movil = req.body.Movil;
-    const email = req.body.Email;
+    const nombre = req.body.nombre;
+    const apellidos = req.body.apellidos;
+    const idFiscal = req.body.idFiscal;
+    const direccion = req.body.direccion;
+    const postal = req.body.postal;
+    const localidad = req.body.localidad;
+    const pais = req.body.pais;
+    const telefono = req.body.telefono;
+    const movil = req.body.movil;
+    const email = req.body.email;
 
-    const sql = "INSERT INTO clientes VALUES(default, ?, ?, ?, ?, ?, ?, ?, ?)";
-    conexionMySQL.query(sql, [nombre, apellidos, nif, cif, direccion, telefono, movil, email], (err) => {
+    const sql = "INSERT INTO clientes VALUES(default, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    conexionMySQL.query(sql, [nombre, apellidos, idFiscal, direccion, postal, localidad, pais, telefono, movil, email], (err) => {
     
         res.json({
             status: 200,
