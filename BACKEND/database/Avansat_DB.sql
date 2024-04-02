d6e7c9ab2cb1accbe3ffa95db5173f9e1a578b37
Josepega <josepegalli@gmail.com>
2024-04-02 13:20:09 +0200

    BDD

--- BACKEND/database/Avansat_DB.sql
+++ BACKEND/database/Avansat_DB.sql
@@ -23,35 +23,34 @@ USE `avansat_db` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `avansat_db`.`clientes` (
   `id_cliente` INT(11) NOT NULL AUTO_INCREMENT,
-  `Tipo_cliente` VARCHAR(45) NOT NULL,
+  `Tipo_cliente` VARCHAR(45) NULL,
   `Nombre` VARCHAR(255) NOT NULL,
-  `Apellidos` VARCHAR(255) NULL DEFAULT NULL,
+  `Apellidos` VARCHAR(255) NULL,
   `Id_fiscal` VARCHAR(12) NOT NULL,
   `Direccion` VARCHAR(255) NOT NULL,
-  `C_postal` INT(5) NULL DEFAULT NULL,
-  `Localidad` VARCHAR(45) NULL DEFAULT NULL,
-  `Pais` VARCHAR(45) NULL DEFAULT NULL,
-  `Telefono` VARCHAR(15) NULL DEFAULT NULL,
-  `Movil` INT(9) NULL DEFAULT NULL,
-  `Email` VARCHAR(45) NULL DEFAULT NULL,
+  `C_postal` VARCHAR(15) NULL DEFAULT 'Desconocido',
+  `Localidad` VARCHAR(45) NULL DEFAULT 'Desconocido',
+  `Pais` VARCHAR(45) NULL DEFAULT 'Desconocido',
+  `Telefono` VARCHAR(15) NULL DEFAULT 'Desconocido',
+  `Movil` VARCHAR(15) NULL DEFAULT 'Desconocido',
+  `Email` VARCHAR(45) NULL,
   PRIMARY KEY (`id_cliente`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 20242
+AUTO_INCREMENT = 20256
 DEFAULT CHARACTER SET = utf8;
 
-select * from clientes;
-
-INSERT INTO clientes (nombre, apellido, edad, email) VALUES ('Juan', 'García', 35, 'juan.garcia@example.com');
-INSERT INTO clientes (nombre, apellido, edad, email) VALUES ('María', 'López', 28, 'maria.lopez@example.com');
-INSERT INTO clientes (nombre, apellido, edad, email) VALUES ('Pedro', 'Martínez', 40, 'pedro.martinez@example.com');
-INSERT INTO clientes (nombre, apellido, edad, email) VALUES ('Laura', 'Hernández', 45, 'laura.hernandez@example.com');
-INSERT INTO clientes (nombre, apellido, edad, email) VALUES ('Carlos', 'Rodríguez', 30, 'carlos.rodriguez@example.com');
-INSERT INTO clientes (nombre, apellido, edad, email) VALUES ('Ana', 'Pérez', 25, 'ana.perez@example.com');
-INSERT INTO clientes (nombre, apellido, edad, email) VALUES ('Luis', 'Gómez', 38, 'luis.gomez@example.com');
-INSERT INTO clientes (nombre, apellido, edad, email) VALUES ('Sofía', 'Díaz', 32, 'sofia.diaz@example.com');
-INSERT INTO clientes (nombre, apellido, edad, email) VALUES ('Elena', 'Fernández', 50, 'elena.fernandez@example.com');
-INSERT INTO clientes (nombre, apellido, edad, email) VALUES ('Miguel', 'Ruiz', 27, 'miguel.ruiz@example.com');
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `avansat_db`.`clientes`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `avansat_db`;
+INSERT INTO `avansat_db`.`clientes` (`id_cliente`, `Tipo_cliente`, `Nombre`, `Apellidos`, `Id_fiscal`, `Direccion`, `C_postal`, `Localidad`, `Pais`, `Telefono`, `Movil`, `Email`) VALUES (DEFAULT, 'Persona', 'Juan Antonio', 'Perales Rocca', '31075705U', 'Gran Via 123 4º 5', '08016', 'Barcelona', 'España', NULL, '654122879', 'info@inglowerst.com');
+INSERT INTO `avansat_db`.`clientes` (`id_cliente`, `Tipo_cliente`, `Nombre`, `Apellidos`, `Id_fiscal`, `Direccion`, `C_postal`, `Localidad`, `Pais`, `Telefono`, `Movil`, `Email`) VALUES (DEFAULT, 'Empresa', 'Gadgets Mgic SL', NULL, 'S87854211H', 'Carrer Ampla 135 Bajo c-2', '08035', 'Barcelona', 'España', '935789789', NULL, NULL);
+
+COMMIT;
+
